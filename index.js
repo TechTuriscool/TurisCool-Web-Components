a68b948a4ed30bcf20e7d9101c8c9003f729ba32
JoseAntonioSegura <jaseggom@adaits.es>
2024-05-02 09:50:45 +0200

    webcomponents

---
+++ index.js
@@ -0,0 +1,4 @@
+import {customButton} from './customButton/customButton.js';
+import {customCard} from './customCard/customCard.js';
+import {customCarousel} from './customCarousel/customCarousel.js';
+import { customUserScore } from './customUserScore/customUserScore.js';
